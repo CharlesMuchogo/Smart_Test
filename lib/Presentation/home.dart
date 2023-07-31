705b373a3bbb396f253d28eb19c88e90194680fd
charles <muchpaul2@gmail.com>
2023-07-31 23:04:49 +0300

    Added instructions

--- lib/Presentation/home.dart
+++ lib/Presentation/home.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:research/Presentation/profile.dart';
 import 'package:research/Presentation/testpage.dart';
+import 'package:video_player/video_player.dart';
 
+import '../functions/constants.dart';
 
 class Homepage extends StatelessWidget {
   Homepage({super.key});
@@ -31,18 +34,26 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home"),
         centerTitle: true,
+        automaticallyImplyLeading: false,
         elevation: 0,
-        actions:  [
+        actions: [
           Padding(
             padding: EdgeInsets.only(right: 20),
             child: GestureDetector(
-              onTap: (){
-                Navigator.push(context, PageTransition(type: PageTransitionType.rightToLeft, child:  Profile()));
-                },
+              onTap: () {
+                Navigator.push(
+                    context,
+                    PageTransition(
+                        type: PageTransitionType.rightToLeft,
+                        child: Profile()));
+              },
               child: CircleAvatar(
                   radius: 20,
                   backgroundColor: Colors.white,
-                  child: Icon(Icons.person, color: Colors.black,)),
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  )),
             ),
           )
         ],
@@ -50,78 +61,171 @@ class Homepage extends StatelessWidget {
       body: Container(
         color: Colors.white,
         child: Center(
+          child: SingleChildScrollView(
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 30, bottom: 30),
-              child: Text(
-                "${greeting()} $firstname",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30, bottom: 50),
-              child: Text(
-                "Please select the test type",
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, PageTransition(type: PageTransitionType.fade, child: TestPage(couples: false,)));
-
-                  },
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 100,
-                        width: 100,
-                        color: Colors.purple.shade100,
-                        alignment: Alignment.center,
-                        child: Icon(
-                          Icons.person,
-                          size: 50,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text("Individual"),
-                      )
-                    ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, bottom: 10),
+                  child: Text(
+                    "${greeting()} $firstname",
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
                 ),
-                GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, PageTransition(type: PageTransitionType.fade, child: TestPage(couples: true,)));
-                  },
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 100,
-                        width: 100,
-                        color: Colors.purple.shade100,
-                        alignment: Alignment.center,
-                        child: Icon(
-                          Icons.people,
-                          size: 50,
-                        ),
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text("Couple"),
-                      )
-                    ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 30),
+                  child: Text(
+                    "Please select the test type",
+                    style: Theme.of(context).textTheme.bodyMedium,
                   ),
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                type: PageTransitionType.fade,
+                                child: TestPage(
+                                  couples: false,
+                                )));
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(color: Colors.purple.shade100,
+                                borderRadius: BorderRadius.circular(8)
+                            ),
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.person,
+                              size: 50,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Individual"),
+                          )
+                        ],
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                type: PageTransitionType.fade,
+                                child: TestPage(
+                                  couples: true,
+                                ),),);
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(color: Colors.purple.shade100,
+                              borderRadius: BorderRadius.circular(8)
+                            ),
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.people,
+                              size: 50,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Couple"),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Instructions",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(decoration: TextDecoration.underline),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                VideoPlayerScreen(
+                  videoUrl: 'assets/video/instructions.mp4',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(INSTRUCTIONS),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset( 'assets/images/pic1.png'),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset( 'assets/images/pic1.png'),
+                ),
+
               ],
-            )
-          ],
-        ),),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class VideoPlayerScreen extends StatefulWidget {
+  final String videoUrl;
+
+  VideoPlayerScreen({required this.videoUrl});
+
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _videoPlayerController;
+  late ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController = VideoPlayerController.asset(widget.videoUrl);
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: false,
+      looping: false,
+    );
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 220,
+      child: Chewie(
+        controller: _chewieController,
       ),
     );
   }
--- lib/functions/constants.dart
+++ lib/functions/constants.dart
@@ -14,6 +14,34 @@ import '../auth/terms.dart';
  const String BASEURL = "http://13.246.207.31:9000";
 const String TermsAndConditions = "https://firebasestorage.googleapis.com/v0/b/flutter-notifications-a462c.appspot.com/o/T%26C%20for%20App.pdf?alt=media&token=b0b8d75d-addd-4451-8d22-37a76dad8d13";
 
+const String INSTRUCTIONS = '''
+You can test for HIV in a safe place
+Self-testing is a convenient and private option for finding out your HIV status. An HIV self-test allows you to test for HIV at home, or wherever you feel most comfortable. This means you don’t have to worry about facing anyone’s judgement for taking a test. And just to reassure you – nobody should be judging anyway and that includes yourself. Taking regular HIV tests is the right thing to do for your health – it deserves a high five!
+
+You can get super speedy results (but you have to test at the right time)
+You can usually get results from a self-test kit within 20 minutes.
+
+Self-test kits work by testing either your saliva (spit) or blood for antibodies to HIV (these are proteins your body makes to try to fight off HIV). Keep in mind that these rapid antibody tests can’t detect HIV straight after infection. For an accurate result you should wait three months after exposure before testing.
+
+Just to remind you, exposure to HIV happens during sex if you don’t use a condom, or the condom breaks.
+
+HIV self-test kits are easy to buy or sometimes provided free by health facilities
+You can buy HIV self-test kits in pharmacies or online and you don’t need a medical note. If you’re happy to go to a clinic you may be able to get them for free. Costs will vary so it can be worth shopping around. As a rough guide, in Kenya-Africa start from around Sh 499 to  999 (just over 3 dollars).
+HIV self-testing is reliable. You can rest assured that an HIV self-test is reliable – the World Health Organization has been recommending it as a safe and effective way to test since 2016. They even say that doing your own is no less accurate than having one done by a trained healthcare worker.
+
+On rare occasions a self-test can produce a positive result which is later found to be incorrect. If you get a positive result from your home test you should go to an HIV clinic as soon as possible. They will always offer you another test to confirm the result and make sure you get the treatment you need to keep you healthy.
+
+An HIV self-test is completely private
+Occasionally, people find themselves in a situation where they personally know their health provider and it feels awkward to go to see them. Or you may worry that your information will not be kept private. In this case an HIV self-test can be an excellent solution.
+
+It’s easy to HIV self-test and it doesn’t hurt
+These infographics give a step-by-step example of what is involved in testing either your blood or your saliva for HIV. Each test kit will have slight variations so always read the full instructions in your kit before beginning.
+
+Pricking your finger to get blood might sound a bit scary – but it really doesn’t hurt – it’s a bit like the ping you can get from an elastic band.
+
+    ''';
+
+
 class AppConstants {
 
    Future<File> loadNetwork() async {
