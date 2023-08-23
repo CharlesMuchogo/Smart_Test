7f5f0211b25a0daf0d8f70722da42313212612ac
Charles Muchogo <muchpaul2@gmail.com>
2023-08-23 14:51:30 +0300

    edit pdf

--- lib/Presentation/documentation.dart
+++ lib/Presentation/documentation.dart
@@ -16,28 +16,34 @@ class Instruction extends StatelessWidget {
           child: Text(FIRST_HEADING, style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.blue.shade800, fontWeight: FontWeight.bold),),
         ),
         Text(FIRST_INSTRUCTIONS,  style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Colors.black, fontWeight: FontWeight.bold)),
+
         Text(SECOND_INSTRUCTIONS),
-        Image.asset("assets/images/pic1.png"),
+        Image.asset("assets/images/pdf1.png"),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(SECOND_HEADING,  style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Colors.black, fontWeight: FontWeight.bold),),
         ),
         Text(STEP_TWO_INSTRUCTIONS),
+        Image.asset("assets/images/pdf2.png"),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(THIRD_HEADING,  style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Colors.black, fontWeight: FontWeight.bold),),
         ),
         Text(STEP_THREE_INSTRUCTIONS),
+        Image.asset("assets/images/pdf3.png"),
+
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(RECOMENDATIONS_HEADING,  style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.blue.shade800, fontWeight: FontWeight.bold),),
         ),
-        Text(RECOMENDATIONS),
-        Text(IMPORTANT_INFORMATION),
+        Text(RECOMENDATIONS, style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Colors.blue.shade800, fontWeight: FontWeight.bold),),
+        Image.asset("assets/images/pdf4.png"),
+        Text(IMPORTANT_INFORMATION, ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(PRECAUTIONS_HEAD,  style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Colors.blue.shade800, fontWeight: FontWeight.bold),),
         ),
+        Image.asset("assets/images/pdf5.png"),
         Text(PRECAUTIONS),
         Padding(
           padding: const EdgeInsets.all(8.0),
