8f115bbe7b66d177a7f5cd6f4be5c00ec69d2dad
charles <muchpaul2@gmail.com>
2025-07-02 18:40:18 +0300

    feat: Added report message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.charlesmuchogo.research"
         minSdk = 24
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.4.0-beta"
+        versionCode = 17
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
